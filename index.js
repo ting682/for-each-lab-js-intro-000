ff0f4fa391fcd88ae764f30d22bdc7bf804e1aa2
Ting Chung <tchung682@gmail.com>
2020-05-13 18:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var even = [2, 4, 6, 8]
 
 function iterativeLog(array){
 
-  array.forEach((index,element) => {
+  array.forEach((array, index, element) => {
     debugger;
     console.log(`${index}` + `:` + `${element}`);
   })
+  
 }
 interativeLog(even);
+
+ 
+evens.forEach((even, index, array) => {
+  debugger;
+  console.log(`${even} is not odd!`);
+});
